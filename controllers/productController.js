9444ad524d519339793606943ca1932effe8e374
etesongg <etesong0114@gmail.com>
2024-10-28 17:38:19 +0900

    페이지네이션

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,5 +1,6 @@
 const Product = require("../models/Product");
 
+const PAGE_SIZE = 5;
 const productController = {};
 
 productController.createProduct = async (req, res) => {
@@ -38,9 +39,18 @@ productController.getProducts = async (req, res) => {
     const { page, name } = req.query;
     const cond = name ? { name: { $regex: name, $options: "i" } } : {}; // 정규화, options insensitive(대소문자 구분x)
     let query = Product.find(cond);
+    let response = { status: "success" };
+
+    if (page) {
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      const totalItemNum = await Product.countDocuments(cond); // count()는 결과의 갯수만 반환함
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
 
     const productList = await query.exec(); // 선언과 실행 분리, 조건을 다 받고 한번에 실행하기 위해
-    res.status(200).json({ status: "success", data: productList });
+    response.data = productList;
+    res.status(200).json(response);
   } catch (e) {
     res.status(400).json({ status: "fail", message: e.message });
   }
